c93aa7853821eed1495fbcfb0e7c05cf568fa52c
owenc216 <owen216413@gmail.com>
2024-09-17 09:20:20 -0400

    Update unit 2.py

--- unit 2.py
+++ unit 2.py
@@ -4,8 +4,8 @@ v = int(input(""))
 if v % 2 == 0:
     print('Even')
 else:
-    print('Odd')
- """
+    print('Odd') """
+
 print("What was the bill?")
 b=int(input(""))
 print("how was the service?" " bad, okay, good or great?")
@@ -14,11 +14,13 @@ q= input("")
 if q == "bad":
     print ("your bill is" ,b*1,  ("no tip included"))
 
-if q == "okay":
-    print ("your bill is" ,b*1.15, ("15 percent tip included") )
+elif q == "okay":
+    print ("your bill is" ,b*1.15, ("15% tip included") )
 
-if q == "good":
-    print ("your bill is" ,b*1.20, ("20 percent tip included"))
+elif q == "good":
+    print ("your bill is" ,b*1.20, ("20% tip included"))
 
-if q == "great":
-    print ("your bill is" ,b*1.25, ("25 percent tip included"))
+elif q == "great":
+
+    print ("your bill is" ,b*1.25, ("25% tip included"))
+
