6a1540099810ab111108ffacbc3cc0a376327aee
owenc216 <owen216413@gmail.com>
2024-09-19 09:20:53 -0400

    Update unit 2.py

--- unit 2.py
+++ unit 2.py
@@ -34,10 +34,20 @@ for i in range(number):
 
 print (Factors)  """
 
-number = input("give me a number")
-number2= input("give me another number")
+number = int(input("give me a number"))
+number2= int(input("give me another number"))
 Factors = []
+CommonF = []
 for i in range(number):
     if number % (i+1)==0:
         Factors.append(i+1)
 
+Factors2 = []
+for i in range(number2):
+    if number % (i+1)==0:
+        Factors2.append(i+1)
+
+if Factors == Factors2:
+    CommonF.append(Factors)
+GCF = CommonF[-1]
+print(GCF)
